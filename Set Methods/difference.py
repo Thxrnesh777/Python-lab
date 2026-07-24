3db889c483d126a0a20235e16f11a824b8d1af39
Thxrnesh777 <tharnesh.webdev@gmail.com>
2026-07-24 14:50:25 +0530

    Create difference.py

---
+++ Set Methods/difference.py
@@ -0,0 +1,11 @@
+a = set()
+n1 = int(input("Enter number of elements in first set: "))
+for i in range(n1):
+    a.add(int(input()))
+
+b = set()
+n2 = int(input("Enter number of elements in second set: "))
+for i in range(n2):
+    b.add(int(input()))
+
+print(a.difference(b))
