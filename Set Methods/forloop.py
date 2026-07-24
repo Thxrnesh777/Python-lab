61030c17499e6118a117e7a3dfbe7922fb6a6cae
Thxrnesh777 <tharnesh.webdev@gmail.com>
2026-07-24 14:40:09 +0530

    Update and rename forloop.py to union.py

--- Set Methods/forloop.py
+++
@@ -1,7 +0,0 @@
-x = set()
-n = int(input("Enter number of elements: "))
-
-for i in range(n):
-    x.add(int(input()))
-
-print(x)
---
+++ Set Methods/union.py
@@ -0,0 +1,13 @@
+a = set()
+n1 = int(input("Enter number of elements in first set: "))
+for i in range(n1):
+    x = int(input())
+    a.add(x)
+
+b = set()
+n2 = int(input("Enter number of elements in second set: "))
+for i in range(n2):
+    x = int(input())
+    b.add(x)
+
+print(a.union(b))
